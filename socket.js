1e4cd770090b9d58050e79b940cb8eff915d5876
Phil Piwonka <philpiwonka@gmail.com>
2014-04-06 01:56:43 +0000

    another button works

--- socket.js
+++ socket.js
@@ -2,9 +2,10 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , Gpio = require('onoff').Gpio
-  , button = new Gpio(27, 'in', 'both');
+  , button = new Gpio(27, 'in', 'both')
+  , button2 = new Gpio(4, 'in', 'both');
 
-io.set('log level', 1);
+//io.set('log level', 1);
 app.listen(8081);
 
 function handler (req, res) {
@@ -28,7 +29,10 @@ button.watch(function(err, value) {
     if (err) exit();
     io.sockets.emit('button', {v:value});
 });
-
+button2.watch(function(err, value) {
+    if (err) exit();
+    io.sockets.emit('button2', {v:value});
+});
 function exit() {
     button.unexport();
     process.exit();
